51a752c47255d45a7f897e6f8edbda2c19baa1a5
Daniel Freire <dgz1606@gmail.com>
2024-02-05 17:59:06 -0500

    Initial Commit

---
+++ Challenge_6_PictureToName/Challenge_6_PictureToNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_6_PictureToNameApp.swift
+//  Challenge_6_PictureToName
+//
+//  Created by Daniel Freire on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_6_PictureToNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
